11978057cffcfdda649e785247d0708be129357c
Roman Grubic <grubic.roman@gmail.com>
2020-01-28 16:24:58 +0000

    Added Darksky API and carousel for future weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,33 @@
-$(document).ready(function() {
-      $("#reset").hide();
-      $("#extra-btn").hide();
+$(document).ready(function () {
+    $("#reset").hide();
+    $("#extra-btn").hide();
 
 });
 
 // --- User's can press enter instead of clicking on submit button --- 
 $("#search").keypress(function (e) {
- var key = e.which;
- if(key == 13)
-  {pullDataByCity();
-    $(".form-inline").hide()
-    $("#current-extra").hide();
-    $("#heading").hide();
-    $("#reset").show();
-    $("#extra-btn").show();
-    $("#reset").removeClass("header")
-    return false;  
-  }
-});   
+    var key = e.which;
+    if (key == 13) {
+        pullDataByCity();
+        $(".form-inline").hide()
+        $("#current-extra").hide();
+        $("#heading").hide();
+        $("#reset").show();
+        $("#extra-btn").show();
+        $("#reset").removeClass("header")
+        return false;
+    }
+});
 
 $("#reload").click(function () {
     window.location.reload();
 })
 
-$("#extra-btn").click(function(){
+$("#extra-btn").click(function () {
     $("#current-extra").fadeToggle();
 })
+
+
 
 function pullDataByCity() {
 
@@ -45,13 +47,15 @@ function pullDataByCity() {
                 humidity,
                 pressure } = data.main;
             const { main } = data.weather[0]
-            const {country,
+            const { country,
                 sunrise,
-                sunset} = data.sys
-            const {all} = data.clouds
-            const {speed} = data.wind
+                sunset } = data.sys
+            const { all } = data.clouds
+            const { speed } = data.wind
+            const { lat,
+                lon } = data.coord
 
-            let timezoneDiff = (timezone/3600);
+            let timezoneDiff = (timezone / 3600);
 
             $("#current").addClass("current")
             var h1 = document.createElement("p");
@@ -64,13 +68,13 @@ function pullDataByCity() {
             cityMain.innerHTML = main.toUpperCase()
             document.getElementById("city-main").appendChild(cityMain);
             var cityDate = document.createElement("p");
-            var nDate = new Date(dt*1000);
+            var nDate = new Date(dt * 1000);
             var timeDiff = timezoneDiff * 60;
             var offsetTime = new Date(nDate.getTime() + timeDiff * 60 * 1000);
             var hours = offsetTime.getHours();
             var dateToString = offsetTime.toDateString();
             var day = dateToString.slice(0, 3);
-            var number = dateToString.slice(4, 10);            
+            var number = dateToString.slice(4, 10);
             var sortDate = day + ", " + number + "<br>" + hours + ":00hrs";
             cityDate.innerHTML = sortDate;
             document.getElementById("city-date").appendChild(cityDate);
@@ -89,7 +93,7 @@ function pullDataByCity() {
             currWind.innerHTML = "Wind speed - " + speed + " m/s";
             document.getElementById("current-extra").appendChild(currWind);
             var currSunrise = document.createElement("p");
-            var currSunRis = new Date(sunrise*1000);
+            var currSunRis = new Date(sunrise * 1000);
             var sunRisDiff = timezoneDiff * 60;
             var realSunRisTime = new Date(currSunRis.getTime() + sunRisDiff * 60 * 1000);
             var currSunRisH = realSunRisTime.getHours();
@@ -97,59 +101,60 @@ function pullDataByCity() {
             currSunrise.innerHTML = "Sunrise at - " + currSunRisH + ":" + currSunRisM + " hrs";
             document.getElementById("current-extra").appendChild(currSunrise);
             var currSunset = document.createElement("p");
-            var currSunSet = new Date(sunset*1000);
+            var currSunSet = new Date(sunset * 1000);
             var sunSetDiff = timezoneDiff * 60;
             var realSunSetTime = new Date(currSunSet.getTime() + sunSetDiff * 60 * 1000);
             var currSunSetH = realSunSetTime.getHours();
             var currSunSetM = realSunSetTime.getMinutes();
             currSunset.innerHTML = "Sunset at - " + currSunSetH + ":" + currSunSetM + " hrs";
             document.getElementById("current-extra").appendChild(currSunset);
-        })
 
-    let apiSearchDays = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=bff58e85aa4f33dcceeb856d37837f05`;
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            let apiSearchDays = `${proxy}https://api.darksky.net/forecast/81bd9bfc27e0a863848c0d003dda4468/${lat},${lon}?units=si`;
 
-    fetch(apiSearchDays)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
+            fetch(apiSearchDays)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
 
-            for (let i = 0; i < 20; i +=2) {
-                const {dt} = data.list[i];
-                const { temp } = data.list[i].main;
-                const {main} = data.list[i].weather[0];
-                const {timezone} = data.city;
+                    for (i = 1; i < 6; i++) {
+                        const { summary,
+                            temperatureHigh,
+                            temperatureLow,
+                            precipProbability,
+                            humidity,
+                            pressure,
+                            windSpeed,
+                            time } = data.daily.data[i];
 
-                let timezoneDiffNextDay = timezone/3600
+                        console.log(data.daily.data[i])
 
-                var div = document.createElement("div");
-                div.setAttribute('id', ["newday" + i])
-                div.setAttribute('class', 'col-5 col-md-2 next-day');
-                document.getElementById("nextday").appendChild(div);
-                var h6 = document.createElement("p")
-                h6.setAttribute('class', 'next-date')
-                var date = new Date(dt * 1000);
-                var timeDiff = timezoneDiffNextDay * 60;
-                var timeDiffNextDay = new Date(date.getTime() + timeDiff * 60 * 1000);
-                var nDate = timeDiffNextDay.toDateString();
-                var hours = timeDiffNextDay.getHours();
-                var day = nDate.slice(0, 3);
-                var number = nDate.slice(4, 10);
-                var nDate = day + ", " + number + "<br>" + hours +":00hrs";
-                h6.innerHTML = nDate;
-                div.appendChild(h6);
-                var tempMed = document.createElement("p");
-                tempMed.setAttribute('class', 'next-tempMed');
-                tempMed.innerHTML = Math.round(temp) + " °C";
-                div.appendChild(tempMed);
-                var summary = document.createElement("p");
-                summary.setAttribute('class', 'next-summary');
-                summary.innerHTML = main.toUpperCase();
-                div.appendChild(summary);                
-            }
+                        var div = document.createElement("div");
+                        div.setAttribute('id', ["newday" + i])
+                        div.setAttribute('class', 'carousel-item new-day');
+                        document.getElementById("carousel").appendChild(div);
+                        var h6 = document.createElement("h6")
+                        var date = new Date(time * 1000);
+                        var nDate = date.toDateString();
+                        var day = nDate.slice(0, 3);
+                        var number = nDate.slice(4, 10);
+                        var nDate = day + ", " + number;
+                        h6.innerHTML = nDate;
+                        div.appendChild(h6);
+                        var h5 = document.createElement("h5");
+                        h5.innerHTML = summary;
+                        div.appendChild(h5);
+                        var par = document.createElement("p")
+                        par.setAttribute('class', 'next-temp')
+                        par.innerHTML = " Temperature <br> Max: " + Math.round(temperatureHigh) + "  °C <br> Min: " + Math.round(temperatureLow) + " °C";
+                        div.appendChild(par);
+                        var parPrecipitation = document.createElement("p")
+                        parPrecipitation.setAttribute('class', 'next-precip')
+                        parPrecipitation.innerHTML = "Rain: " + Math.round(precipProbability * 100) + " %"
+                        div.appendChild(parPrecipitation);
+                    }
+                })
         })
 };
-
-
-
